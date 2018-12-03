3c4adcd6cf051ecbe4251c72f1894009f5395fbc
John Pham <hprobotic@gmail.com>
2018-12-03 23:19:54 +0700

    Add day 2 part 1 solution

--- src/day2/inventory_management_system.py
+++ src/day2/inventory_management_system.py
@@ -1,14 +1,25 @@
+from collections import Counter
 from src.input import GetInput
 
 INPUT = GetInput(2)
+input = INPUT.value.split()
 
 
 def first():
-    return str('First')
+    ids_double = 0
+    ids_tripple = 0
+    for id in input:
+        counts = set(Counter(id).values())
+        if 2 in counts:
+            ids_double += 1
+        if 3 in counts:
+            ids_tripple += 1
+
+    return ids_double * ids_tripple
 
 
 def last():
-    return str('Last')
+    return 'Last'
 
 
 if __name__ == '__main__':
